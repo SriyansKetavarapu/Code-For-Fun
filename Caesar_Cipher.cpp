ef16b1f7b08ea9ffd3099ea6fe84d68c48ade4f6
SriyansKetavarapu <119074545+SriyansKetavarapu@users.noreply.github.com>
2022-11-26 00:18:23 +0530

    Create Caesar_Cipher.cpp
    This is a small program showing the basics of the Caesar Cipher as a C++ code

---
+++ Caesar_Cipher.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+char cipher[100];
+
+void encode(char* arr, int key){
+
+    int i=0;
+    for (i = 0; i < strlen(arr); i++)
+    {
+        cipher[i] = arr[i] + (key%26);
+    }
+    cipher[i] = '\0';
+    cout<<cipher<<endl;
+
+}
+void decode(int key){
+
+    char res[100];
+    int i;
+    for (i = 0; i < strlen(cipher); i++)
+    {
+        res[i] = cipher[i] - (key%26);
+    }
+    res[i] = '\0';
+    cout<<res<<endl;
+
+}
+
+int main(){
+
+char arr[100];
+cout<<"Enter the string to be encoded ";
+cin>>arr;
+int key;
+cout<<"Enter the key ";
+cin>>key;
+cout<<"The encoded cipher is - ";
+encode(arr, 13);
+cout<<"The decoded message is - ";
+decode(13);
+cout<<endl;
+return 0;
+}
